96c2b1ae32aaab5a89ac800a190fb7357ac4878d
scimarl <feliyax@gmail.com>
2020-05-03 19:42:38 +1000

    Finish
    Done the test and all the stuffs done

--- src/components/InitialPage/Grid.tsx
+++ src/components/InitialPage/Grid.tsx
@@ -1,6 +1,5 @@
 import { ColumnDirective, ColumnsDirective, GridComponent, Inject, Sort, Filter, FilterSettingsModel, IFilter } from '@syncfusion/ej2-react-grids';
 import { DetailRow, Grid, GridModel } from '@syncfusion/ej2-react-grids';
-import { DataManager, Query } from '@syncfusion/ej2-data';
 import * as React from 'react';
 import { data } from './data';
 
@@ -29,9 +28,8 @@ export default class Grids extends React.Component<{}, {}>{
         <ColumnsDirective >
             <ColumnDirective field='name' filter={this.Filter} width='100' />
             <ColumnDirective field='region' filter={this.Filter}  width='100'/>
-            <ColumnDirective field='price' width='100' textAlign="Right"/>
-            <ColumnDirective field='rate' width='100' format="C2" textAlign="Right"/>
-            <ColumnDirective field='action' width='100'/>
+            <ColumnDirective field='price' width='100' format="C2" textAlign="Right"/>
+            <ColumnDirective field='rate' width='100' textAlign="Right"/>
         </ColumnsDirective>
         <Inject services={[Sort,Filter,DetailRow]} />
     </GridComponent>
--- src/components/InitialPage/data.tsx
+++ src/components/InitialPage/data.tsx
@@ -1,11 +1,11 @@
-export let data: Object[] = [{id:1, name:'Apple', region:'Australia', price:'10', rate:'4', onStock:1, requirement:0, explanation:'red apple' },
-        {id:2, name:'Apple', region:'America', price:'10', rate:'3',  onStock:1, requirement:0, explanation:'green apple'},
-        {id:3, name:'Apple', region:'China', price:'15', rate:'5',  onStock:1, requirement:0, explanation:'delicious apple!' },
-        {id:4, name:'Pear', region:'Australia', price:'20', rate:'2',  onStock:1, requirement:0, explanation:'sour'},
-        {id:5, name:'Pear', region:'China', price:'10', rate:'4',  onStock:1, requirement:0, explanation:'sweet'},
-        {id:6, name:'Berry', region:'Australia', price:'30', rate:'5',  onStock:1, requirement:0, explanation:'purple'},
-        {id:7, name:'Berry', region:'Japan', price:'25', rate:'4',  onStock:1, requirement:0, explanation:'blue-purple'},
-        {id:8, name:'Berry', region:'Italy', price:'35', rate:'1',  onStock:1, requirement:0, explanation:'purple-blue'},
-        {id:9, name:'Berry', region:'China', price:'60', rate:'3',  onStock:1, requirement:0, explanation:'blue'},
-        {id:10, name:'Pineapple', region:'Amarica', price:'50', rate:'4',  onStock:1, requirement:0, explanation:'eat after boil!'}
+export let data: Object[] = [{id:1, name:'Apple', region:'Australia', price:'10.00', rate:'4', onStock:1, requirement:0, explanation:'red apple' },
+        {id:2, name:'Apple', region:'America', price:'10.00', rate:'3',  onStock:1, requirement:0, explanation:'green apple'},
+        {id:3, name:'Apple', region:'China', price:'15.00', rate:'5',  onStock:1, requirement:0, explanation:'delicious apple!' },
+        {id:4, name:'Pear', region:'Australia', price:'20.00', rate:'2',  onStock:1, requirement:0, explanation:'sour'},
+        {id:5, name:'Pear', region:'China', price:'10.00', rate:'4',  onStock:1, requirement:0, explanation:'sweet'},
+        {id:6, name:'Berry', region:'Australia', price:'30.00', rate:'5',  onStock:1, requirement:0, explanation:'purple'},
+        {id:7, name:'Berry', region:'Japan', price:'25.00', rate:'4',  onStock:1, requirement:0, explanation:'blue-purple'},
+        {id:8, name:'Berry', region:'Italy', price:'35.00', rate:'1',  onStock:1, requirement:0, explanation:'purple-blue'},
+        {id:9, name:'Berry', region:'China', price:'60.00', rate:'3',  onStock:1, requirement:0, explanation:'blue'},
+        {id:10, name:'Pineapple', region:'Amarica', price:'50.00', rate:'4',  onStock:1, requirement:0, explanation:'eat after boil!'}
     ];
